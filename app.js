aa3bbd3d69746d0fb308c028693fc82d69c7a3bc
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2020-07-25 12:45:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express=require("express");
+const app=express();
+
+app.use(express.json({extended:false}));
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server running at ${PORT}`))
